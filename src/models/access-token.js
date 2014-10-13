179063c8a6590c8fb9ab640f791fbfdbff013b73
88z <paperairlane89@gmail.com>
2014-10-13 18:11:38 +0400

    Модель Access Token

--- src/models/access-token.js
+++ src/models/access-token.js
@@ -7,17 +7,20 @@ var _ = require('lodash');
 var uuid = require('node-uuid');
 var orm = require('orm');
 var extensions = require('./extensions');
+var config = require('../config');
 
 exports.setup = function(db) {
 
     var methods = extensions('serialize', {
-        generateToken: function() {
-            return uuid.v4();
-        }
+        expired: function(){
+            return this.expirationDate < new Date();
+        },
     });
 
     var AccessToken = db.define('accessToken', {
-        token: { type: 'text', required: true }
+        value: { type: 'text', required: true },
+        refreshToken: {type: 'text', required: true},
+        expirationDate: {type: 'date', time: true}
     }, {
         methods: methods,
 
@@ -25,14 +28,30 @@ exports.setup = function(db) {
         }
     });
 
-    AccessToken.generateToken = methods.generateToken;
+    AccessToken.generateToken = function() {
+            return uuid.v4();
+    };
+
+    AccessToken.generateExpirationDate = function(){
+        var expiresIn = config.token.expiresIn;
+        var date = new Date();
+        date.setSeconds(date.getSeconds()+expiresIn);
+        return date;
+    };
+
+    //Не работает
+    AccessToken.clearRefresh = function(refreshToken){
+        AccessToken.find({refreshToken: refreshToken}, function(err, tokens){
+            tokens.forEach(function(token){
+                token.delete();
+            })
+        })
+    };
 
     return AccessToken;
 };
 
 exports.setupRelations = function(AccessToken, models) {
-
-    AccessToken.hasOne('user', models.user, { required: true, reverse: 'accessTokens', autoFetch: false });
-    AccessToken.hasOne('apiClient', models.apiClient, { required: true, reverse: 'accessTokens', autoFetch: false });
-
+    AccessToken.hasOne('user',         models.user,         { required: true, reverse: 'accessTokens', autoFetch: false });
+    AccessToken.hasOne('apiClient',    models.apiClient,    { required: true, reverse: 'accessTokens', autoFetch: false });
 };
