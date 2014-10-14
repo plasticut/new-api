27a03bfc46191ead9100560e579b8819056c00d1
88z <paperairlane89@gmail.com>
2014-10-14 15:11:30 +0400

    Тесты получения токена по client name, client password, username, user password

--- test/cases/requests/authorization/tokens.js
+++ test/cases/requests/authorization/tokens.js
@@ -7,17 +7,108 @@ var async = require('async');
 var request = require('supertest');
 var expect = chai.expect;
 var assert = chai.assert;
-var fixturesPath = config.fixturesPath;
+var path = require('path');
 
 describe('Token requests', function(){
     var self = this;
-//    it()
-    describe('POST oauth2/token route', function(){
+    self.fixtPath = path.resolve(__dirname+'/fixtures')
+    before(function(done){
+        async.waterfall([
+            dbhelper.setup,
+            function(database, callback){
+                dbhelper.truncate(['user', 'apiClient'], function(err){
+                    callback(err, database);
+                });
+            },
+            function(database, callback){
+                self.models = database.models;
+                callback(null, new FixtLoader(database))
+            },
+            function(fxl, callback){
+                self.fxl = fxl;
+                fxl.fromFolder(self.fixtPath, callback);
+            }
+        ], function(err, inserted){
+            if(err){
+                return done(err);
+            }
+            done()
+        })
+    });
+
+    describe('POST oauth/token route', function(){
+        self.url = '/oauth/token';
+        var authHeader = function(name, password){
+            return 'Basic ' + new Buffer(name + ':' + password).toString('base64');
+        }
         describe('recieving tokens by username/password', function(){
-            it("should respond with code 403 if no authentication data were sended");
-            it("should respond with code 403 if username/password incorrect");
-            it("should respond with code 403 if clientId/clientSecret incorrect");
-            it("should respond with tokens if username/password and clientid/clientsecret are correct");
+            it("should respond with code 401 if no authentication data were sended", function(done){
+                request(app).post(self.url)
+                    .type('form')
+                    .expect(401, {}, done);
+            });
+            it("should respond with code 401 if client name incorrect", function(done){
+                request(app).post(self.url)
+                    .type('form')
+                    .set('Authorization', authHeader('givemetoken', 'demo'))
+                    .expect(401, {}, done);
+            });
+
+            it("should respond with code 401 if client password incorrect", function(done){
+                request(app).post(self.url)
+                    .type('form')
+                    .set('Authorization', authHeader('new-ui', 'givemetoken'))
+                    .expect(401, {}, done);
+            });
+
+            it("should respond with code 403 if username incorrect", function(done){
+                request(app).post(self.url)
+                    .type('form')
+                    .set('Authorization', authHeader('new-ui', 'demo'))
+                    .send({username: 'vasya1', password:'eloper', grant_type: 'password'})
+                    .expect(403,
+                        {
+                            error: 'invalid_grant',
+                            error_description: 'Invalid resource owner credentials'
+                        },
+                    done);
+            });
+            it("should respond with code 403 if password incorrect", function(done){
+                request(app).post(self.url)
+                    .type('form')
+                    .set('Authorization', authHeader('new-ui', 'demo'))
+                    .send({username: 'vasya', password:'eloper1', grant_type: 'password'})
+                    .expect(403,
+                        {
+                            error: 'invalid_grant',
+                            error_description: 'Invalid resource owner credentials'
+                        },
+                    done);
+            });
+
+            it("should respond with code 501 if grant_type incorrect", function(done){
+                request(app).post(self.url)
+                    .type('form')
+                    .set('Authorization', authHeader('new-ui', 'demo'))
+                    .send({username: 'vasya', password:'eloper', grant_type: 'password1'})
+                    .expect(501,
+                        {
+                            error: 'unsupported_grant_type',
+                            error_description: 'Unsupported grant type: password1'
+                        },
+                    done);
+            });
+            it("should respond with tokens if username/password and clientid/clientsecret are correct", function(done){
+                request(app).post(self.url)
+                    .type('form')
+                    .set('Authorization', authHeader('new-ui', 'demo'))
+                    .send({username: 'vasya', password:'eloper', grant_type: 'password'})
+                    .expect(200)
+                    .end(function(err, res){
+                        expect(res.body).to.include.keys('refresh_token', 'access_token');
+                        done();
+                    });
+            });
         });
         describe("recieving access token by refresh token", function(){
             it("should respond with code 403 if refresh token incorrect");
